01179143cb78db3d34f80d16a761d77ff9d130ae
李明月 <limingyue5@xiaomi.com>
2021-07-18 22:04:21 +0800

    优化

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,23 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutterapp/main.dart';
 
 void main() {
-  group("group test", () {
-    setUp(() {
-      print('set up');
-    });
-
-    tearDown(() {
-      print('tear down');
-    });
-
-    test('test 1', () {
-      int i = 0;
-      expect(i, 0);
-    });
-
-    test('test 2', () {
-      int i = 1;
-      expect(i, 1);
-    });
+  //第一个用例，判断Counter对象调用increase方法后是否等于1
+  test('Increase a counter value should be 1', () {
+    final counter = Counter();
+    counter.increase();
+    expect(counter.count, 1);
+  });
+  //第二个用例，判断1+1是否等于2
+  test('1+1 should be 2', () {
+    expect(1 + 1, 2);
   });
 }
